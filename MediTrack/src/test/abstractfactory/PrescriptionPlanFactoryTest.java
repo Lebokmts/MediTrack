2807ed8359d1fb40ee6c4e8d2dcb207ec64cd1ed
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:39:11 +0200

    Update PrescriptionPlanFactoryTest.java

--- MediTrack/src/test/abstractfactory/PrescriptionPlanFactoryTest.java
+++ MediTrack/src/test/abstractfactory/PrescriptionPlanFactoryTest.java
@@ -1,12 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package test;
 
-/**
- *
- * @author Kelebogile
- */
+import abstractfactory.PrescriptionPlanFactory;
+import abstractfactory.StandardPrescriptionFactory;
+import models.Prescription;
+import models.Reminder;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PrescriptionPlanFactoryTest {
-    
+    @Test
+    public void testStandardFactoryCreatesBothObjects() {
+        PrescriptionPlanFactory factory = new StandardPrescriptionFactory();
+        Prescription rx = factory.createPrescription();
+        Reminder rem = factory.createReminder();
+
+        assertNotNull(rx);
+        assertNotNull(rem);
+    }
 }
