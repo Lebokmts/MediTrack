3d5589224e461612f4e3305950e677cbbccde0c6
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:23:15 +0200

    Update PrescriptionFactory.java

--- MediTrack/src/factorymethod/PrescriptionFactory.java
+++ MediTrack/src/factorymethod/PrescriptionFactory.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package factorymethod;
 
 import models.Prescription;
-/**
- *
- * @author Kelebogile
- */
+
 public abstract class PrescriptionFactory {
      public abstract Prescription createPrescription();
 }
