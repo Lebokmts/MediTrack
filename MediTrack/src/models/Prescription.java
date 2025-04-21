836891244da3595a953de9f5b224227475a6b601
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:19:20 +0200

    Update Prescription.java

--- MediTrack/src/models/Prescription.java
+++ MediTrack/src/models/Prescription.java
@@ -1,15 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 import prototypes.PrescriptionPrototype;
-/**
- *
- * @author Kelebogile
- */
-
-
 
 public class Prescription implements PrescriptionPrototype {
     // (keep your attributes and methods as is...)
