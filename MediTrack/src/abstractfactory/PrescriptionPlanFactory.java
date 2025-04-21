abfbbd721373830ff09ef56cae8c89731e9b999b
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:21:16 +0200

    Update PrescriptionPlanFactory.java

--- MediTrack/src/abstractfactory/PrescriptionPlanFactory.java
+++ MediTrack/src/abstractfactory/PrescriptionPlanFactory.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package abstractfactory;
 import models.Prescription;
 import models.Reminder;
-/**
- *
- * @author Kelebogile
- */
 
 public interface PrescriptionPlanFactory {
     Prescription createPrescription();
