4e2637050165eb53e81e53d9b45fa6396fffe56b
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:19:40 +0200

    Update QRCode.java

--- MediTrack/src/models/QRCode.java
+++ MediTrack/src/models/QRCode.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package models;
 
 import java.time.LocalDateTime;
-/**
- *
- * @author Kelebogile
- */
+
 public class QRCode {
     private String qrID;
     private String encodedData;
