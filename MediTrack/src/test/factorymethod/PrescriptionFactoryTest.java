392a9c35e0197c96b306e3e81ef11f615b5342c0
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:40:02 +0200

    Update PrescriptionFactoryTest.java

--- MediTrack/src/test/factorymethod/PrescriptionFactoryTest.java
+++ MediTrack/src/test/factorymethod/PrescriptionFactoryTest.java
@@ -1,4 +1,17 @@
+package test;
+
+import factorymethod.BasicPrescriptionFactory;
+import factorymethod.PrescriptionFactory;
+import models.Prescription;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PrescriptionFactoryTest {
-    
+    @Test
+    public void testCreatePrescription() {
+        PrescriptionFactory factory = new BasicPrescriptionFactory();
+        Prescription p = factory.createPrescription();
+        assertNotNull(p);
+        assertEquals("RX001", p.getPrescriptionID());
+    }
 }
