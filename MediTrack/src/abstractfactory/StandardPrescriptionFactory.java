613e59a7e899d79e79d7908040070c60576ff8ac
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:21:32 +0200

    Update StandardPrescriptionFactory.java

--- MediTrack/src/abstractfactory/StandardPrescriptionFactory.java
+++ MediTrack/src/abstractfactory/StandardPrescriptionFactory.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package abstractfactory;
 
 import models.Prescription;
 import models.Reminder;
 import java.time.LocalDateTime;
-
-/**
- *
- * @author Kelebogile
- */
 
 public class StandardPrescriptionFactory implements PrescriptionPlanFactory {
     @Override
