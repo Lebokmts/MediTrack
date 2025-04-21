e3eda3b4e2349ebcba581f7b2b763c917dd8ccbd
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:40:31 +0200

    Update PrescriptionPrototypeTest.java

--- MediTrack/src/test/models/PrescriptionPrototypeTest.java
+++ MediTrack/src/test/models/PrescriptionPrototypeTest.java
@@ -1,12 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package test;
 
-/**
- *
- * @author Kelebogile
- */
+import models.Prescription;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PrescriptionPrototypeTest {
-    
+    @Test
+    public void testCloneCreatesNewPrescription() {
+        Prescription original = new Prescription("RX_ORIG", "2024-04-19", "2024-05-01", "Created");
+        Prescription copy = original.clone();
+
+        assertNotNull(copy);
+        assertNotSame(original, copy);
+        assertTrue(copy.getPrescriptionID().contains("COPY"));
+    }
 }
