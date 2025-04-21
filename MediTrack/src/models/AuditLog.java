6599d191a4d499eba1264f29f6c2b2676fe22b71
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:17:37 +0200

    Update AuditLog.java

--- MediTrack/src/models/AuditLog.java
+++ MediTrack/src/models/AuditLog.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package models;
 
 import java.time.LocalDateTime;
-/**
- *
- * @author Kelebogile
- */
+
 public class AuditLog {
     private String logID;
     private String action;
