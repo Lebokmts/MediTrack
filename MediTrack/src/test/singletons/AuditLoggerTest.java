698a33a78f078c6a71ab2bb17a2f944f795382c6
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:41:33 +0200

    Update AuditLoggerTest.java

--- MediTrack/src/test/singletons/AuditLoggerTest.java
+++ MediTrack/src/test/singletons/AuditLoggerTest.java
@@ -1,4 +1,15 @@
+package test;
+
+import singletons.AuditLogger;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AuditLoggerTest {
-    
+    @Test
+    public void testSingletonLoggerReturnsSameInstance() {
+        AuditLogger logger1 = AuditLogger.getInstance();
+        AuditLogger logger2 = AuditLogger.getInstance();
+
+        assertSame(logger1, logger2);
+    }
 }
