1f1ccbe9c4cad9109e4997db8960f834cf3b02f1
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-04-21 02:41:01 +0200

    Update ReminderBuilderTest.java

--- MediTrack/src/test/models/ReminderBuilderTest.java
+++ MediTrack/src/test/models/ReminderBuilderTest.java
@@ -1,12 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package test;
 
-/**
- *
- * @author Kelebogile
- */
+import models.Reminder;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ReminderBuilderTest {
-    
+    @Test
+    public void testReminderBuilderCreatesReminder() {
+        Reminder reminder = new Reminder.ReminderBuilder("REM_BUILDER_1", LocalDateTime.now())
+                                .setRecurring(true)
+                                .setNote("Test Reminder")
+                                .build();
+
+        assertNotNull(reminder);
+        assertEquals("REM_BUILDER_1", reminder.getReminderID());
+    }
 }
