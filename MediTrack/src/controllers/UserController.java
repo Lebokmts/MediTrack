81ef944136b8eddbd9ab11b6fdcc6077956e0d12
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-05-01 12:55:14 +0200

    Update UserController.java

--- MediTrack/src/controllers/UserController.java
+++ MediTrack/src/controllers/UserController.java
@@ -1,12 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package controllers;
 
-/**
- *
- * @author Kelebogile
- */
+import com.google.gson.Gson;
+import models.Doctor;
+import models.User;
+import repositories.inmemory.InMemoryUserRepository;
+import services.UserService;
+
+import static spark.Spark.*;
+
 public class UserController {
-    
+
+    public static void initRoutes() {
+        Gson gson = new Gson();
+        UserService userService = new UserService(new InMemoryUserRepository());
+
+        // Register user
+        post("/api/users", (req, res) -> {
+            User user = gson.fromJson(req.body(), Doctor.class); // Defaulting to Doctor for now
+            userService.registerUser(user);
+            res.status(201);
+            return "User registered";
+        });
+
+        // Get all users
+        get("/api/users", (req, res) -> {
+            res.type("application/json");
+            return gson.toJson(userService.getAllUsers());
+        });
+
+        // Get user by ID
+        get("/api/users/:id", (req, res) -> {
+            User user = userService.getUserById(req.params("id"))
+                    .orElseThrow(() -> new Exception("User not found"));
+            res.type("application/json");
+            return gson.toJson(user);
+        });
+
+        // Delete user
+        delete("/api/users/:id", (req, res) -> {
+            userService.deleteUser(req.params("id"));
+            return "User deleted";
+        });
+    }
 }
+
