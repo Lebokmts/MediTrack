b5d23caf4add418201337bfc75812ba2d8e76713
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-05-01 12:50:55 +0200

    Update UserService.java

--- MediTrack/src/services/UserService.java
+++ MediTrack/src/services/UserService.java
@@ -1,12 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package services;
 
-/**
- *
- * @author Kelebogile
- */
+import models.User;
+import repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class UserService {
-    
+    private final UserRepository userRepo;
+
+    public UserService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public void registerUser(User user) {
+        userRepo.save(user);
+    }
+
+    public Optional<User> getUserById(String id) {
+        return userRepo.findById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    public void deleteUser(String id) {
+        userRepo.delete(id);
+    }
 }
+
+
