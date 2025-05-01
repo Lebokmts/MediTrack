47e6a66e1a34f2c437c1f8d404de0841ae274c33
Lebokmts <117479569+Lebokmts@users.noreply.github.com>
2025-05-01 12:53:48 +0200

    Update PrescriptionController.java

--- MediTrack/src/controllers/PrescriptionController.java
+++ MediTrack/src/controllers/PrescriptionController.java
@@ -1,12 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package controllers;
 
-/**
- *
- * @author Kelebogile
- */
+import com.google.gson.Gson;
+import models.Prescription;
+import repositories.inmemory.InMemoryPrescriptionRepository;
+import services.PrescriptionService;
+
+import static spark.Spark.*;
+
 public class PrescriptionController {
-    
+
+    public static void initRoutes() {
+        Gson gson = new Gson();
+        PrescriptionService service = new PrescriptionService(new InMemoryPrescriptionRepository());
+
+        // Create
+        post("/api/prescriptions", (req, res) -> {
+            Prescription p = gson.fromJson(req.body(), Prescription.class);
+            service.createPrescription(p);
+            res.status(201);
+            return "Prescription created";
+        });
+
+        // Get all
+        get("/api/prescriptions", (req, res) -> {
+            res.type("application/json");
+            return gson.toJson(service.getAllPrescriptions());
+        });
+
+        // Get by ID
+        get("/api/prescriptions/:id", (req, res) -> {
+            Prescription p = service.getPrescriptionById(req.params("id"))
+                    .orElseThrow(() -> new Exception("Not found"));
+            res.type("application/json");
+            return gson.toJson(p);
+        });
+
+        // Verify prescription
+        put("/api/prescriptions/:id/verify", (req, res) -> {
+            service.verifyPrescription(req.params("id"));
+            return "Prescription verified.";
+        });
+
+        // Delete
+        delete("/api/prescriptions/:id", (req, res) -> {
+            service.deletePrescription(req.params("id"));
+            return "Prescription deleted.";
+        });
+    }
 }
+
+
